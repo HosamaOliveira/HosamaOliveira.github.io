2b0d0d0b400efce1e67827dad2eafb3ec97a912c
HosamaOliveira <hosama17@gmail.com>
2022-02-23 23:52:26 -0300

    Primeiro commit, novo design

--- main.js
+++ main.js
@@ -10,19 +10,32 @@ function getGitHubProfileInfos() {
   .then(data => {
     userImage.src = data.avatar_url
     userName.textContent = data.name
+    bioSideMenu.textContent = data.bio
     userBio.textContent = data.bio
+    locationSideMenu.textContent = data.location
     locat.textContent = data.location
   })
 }
 
 getGitHubProfileInfos()
 
+// Função para mostrar a idade correta na tela
+let showAge = document.querySelector("#showAge")
+let yearOfBirth = new Date(1993,12,23)
+
+let data = new Date()
+let fullDate  = new Date(data.getTime() - yearOfBirth.getTime())
+let age = fullDate.getFullYear()-1970 
+
+showAge.innerHTML = ` ${age} years`
+
+
 // Animar todos os itens na tela que tem o atributo data-anime
 
 const item = document.querySelectorAll("[data-anime]")
 
 const animeScroll = () => {
-  const windowTop = window.pageYOffset + window.innerHeight * 0.90
+  const windowTop = window.pageYOffset + window.innerHeight * 0.01
 
   item.forEach((element) => {
     if (windowTop > element.offsetTop) {
